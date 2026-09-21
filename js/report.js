56f72433a83c37e0f25237a4245bd9cf91a0a589
tuanenv <tuanenv@hotmail.com>
2026-09-21 11:50:31 +0700

    Update report.js

--- js/report.js
+++ js/report.js
@@ -74,11 +74,9 @@ function createNewsCard(item, index) {
   return `
     <article class="card card-${signpost.toLowerCase()}">
 
-      <div class="card-image-wrapper" data-news-index="${index}" style="position: relative;">
-        <img class="card-image" data-news-index="${index}" src="${escapeHtml(item.image)}"on type="button" class="btn-change-image" data-news-index="${index}" style="display: none;" title="เปลี่ยนรูปภาพ">
-          🖼️ เปลี่ยนรูป
-        </button>
-      </div>
+<div class="card-image-wrapper">
+${escapeHtml(item.image)}escapeHtml(item.title)}" loading="eager">
+</div>
 
 
       <div class="card-meta">
